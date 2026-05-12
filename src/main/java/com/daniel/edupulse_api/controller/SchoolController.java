43cd43ce81546b209461b09d2f17fa69629dc87e
dlzada <danielluiz1607@gmail.com>
2026-05-12 14:29:19 -0300

    feat: creation route post in SchoolController

--- src/main/java/com/daniel/edupulse_api/controller/SchoolController.java
+++ src/main/java/com/daniel/edupulse_api/controller/SchoolController.java
@@ -4,10 +4,7 @@ import com.daniel.edupulse_api.dto.SchoolDTO;
 import com.daniel.edupulse_api.service.SchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,11 @@ public class SchoolController {
     @ResponseStatus(HttpStatus.OK)
     public List<SchoolDTO> getAll(){
         return service.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SchoolDTO create(@RequestBody SchoolDTO dto){
+        return service.save(dto);
     }
 }
