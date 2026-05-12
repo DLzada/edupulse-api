ce8cb111a99bf4a50c2f7a69ced10549a099e790
dlzada <danielluiz1607@gmail.com>
2026-05-12 14:13:51 -0300

    feat: creation method save in SchoolService

--- src/main/java/com/daniel/edupulse_api/service/SchoolService.java
+++ src/main/java/com/daniel/edupulse_api/service/SchoolService.java
@@ -18,4 +18,26 @@ public class SchoolService {
                 .map(s -> new SchoolDTO(s.getInepCode(), s.getName(), s.getCity(), s.getState(), s.isHasInternet(), s.getStudentCount()))
                 .toList();
     }
+
+    public SchoolDTO save(SchoolDTO dto){
+        School school = School.builder()
+                .inepCode(dto.inepCode())
+                .name(dto.name())
+                .city(dto.city())
+                .state(dto.state())
+                .hasInternet(dto.hasInternet())
+                .studentCount(dto.studentCount())
+                .build();
+
+        School savedSchool = schoolRepository.save(school);
+
+        return new SchoolDTO(
+                savedSchool.getInepCode(),
+                savedSchool.getName(),
+                savedSchool.getCity(),
+                savedSchool.getState(),
+                savedSchool.isHasInternet(),
+                savedSchool.getStudentCount()
+        );
+    }
 }
