215c61841fe6bfb84c58f06d63dc19bad47caf23
dlzada <danielluiz1607@gmail.com>
2026-05-12 18:18:47 -0300

    feat: creation resourceNotFound in ResourceExceptionHandler

--- src/main/java/com/daniel/edupulse_api/infra/exception/ResourceExceptionHandler.java
+++ src/main/java/com/daniel/edupulse_api/infra/exception/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
@@ -20,5 +22,18 @@ public class ResourceExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<StandardError> resourceNotFound(NoSuchElementException e, HttpServletRequest request){
+        StandardError err = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Rsource Not Found",
+                "O recurso solicitado não foi encontrado no sistema.",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 }
