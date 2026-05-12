e66e645a9ac9db4bd18e6a7faea0bb05983c11cb
dlzada <danielluiz1607@gmail.com>
2026-05-12 08:38:16 -0300

    initial commit

---
+++ src/main/java/com/daniel/edupulse_api/EdupulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.edupulse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupulseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/edupulse_api/EdupulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.edupulse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdupulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
