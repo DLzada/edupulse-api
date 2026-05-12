1316f8a7251bf3c4da7c9739cb5bd6ed393a9092
dlzada <danielluiz1607@gmail.com>
2026-05-12 09:03:07 -0300

    feat: add method for seach all the school of a city in repository

--- src/main/java/com/daniel/edupulse_api/database/repository/SchoolRepository.java
+++ src/main/java/com/daniel/edupulse_api/database/repository/SchoolRepository.java
@@ -3,7 +3,9 @@ package com.daniel.edupulse_api.database.repository;
 import com.daniel.edupulse_api.database.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SchoolRepository extends JpaRepository<School, UUID> {
+    List<School> findByCityIgnoreCase(String city);
 }
