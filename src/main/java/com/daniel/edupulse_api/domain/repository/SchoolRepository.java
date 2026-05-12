ef8b881729b8e6c756100751e95bc754999c245a
dlzada <danielluiz1607@gmail.com>
2026-05-12 15:21:41 -0300

    feat: creation method for seach city and level

--- src/main/java/com/daniel/edupulse_api/domain/repository/SchoolRepository.java
+++ src/main/java/com/daniel/edupulse_api/domain/repository/SchoolRepository.java
@@ -1,6 +1,7 @@
 package com.daniel.edupulse_api.domain.repository;
 
 import com.daniel.edupulse_api.domain.model.School;
+import com.daniel.edupulse_api.domain.model.SchoolLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 public interface SchoolRepository extends JpaRepository<School, UUID> {
     List<School> findByCityIgnoreCase(String city);
+
+    List<School> findByCityIgnoreCaseAndLevel(String city, SchoolLevel level);
 }
